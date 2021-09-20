e7806c4a8635d2127b218dfe4af5d5652a6c2d52
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-20 10:19:36 +0300

    store the token in the localStroge

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,16 +1,18 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { AuthContext } from '../../store/auth-contex';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
 	const AuthCtx = useContext(AuthContext);
+	const history = useHistory();
 
 	const isLoggedIn = AuthCtx.isLoggedIn;
 
 	const lougoutHandler = () => {
 		AuthCtx.logout();
+		history.push('/auth');
 	};
 
 	return (
